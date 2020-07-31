4755ce18f33212fb0b49f8a5479ac7e9fa5f9a42
Justin Haze <jhaze@ntia.doc.gov>
2020-07-31 08:41:40 -0600

    remove logger - handled in plugin instead

--- src/sensor/settings.py
+++ src/sensor/settings.py
@@ -329,7 +329,6 @@ LOGGING = {
         "tasks": {"handlers": ["console"], "level": LOGLEVEL},
         "scos_actions": {"handlers": ["console"], "level": LOGLEVEL},
         "scos_usrp": {"handlers": ["console"], "level": LOGLEVEL},
-        "scos_sensor_keysight": {"handlers": ["console"], "level": LOGLEVEL},
     },
 }
 
